2d30696847b5db9a532d3d72b3f8207aebdfbb22
Madhur <madhurthakkar247@gmail.com>
2023-02-09 23:11:03 +0530

    get yolov5 output file created

---
+++ post_processingv5.py
@@ -0,0 +1,1 @@
+from yolov5.yolov5_output import run_yolov5
--- yolov5/yolov5_output.py
+++ yolov5/yolov5_output.py
@@ -1,5 +1,5 @@
 import torch
-# import torchvision
+import cv2
 import os
 from models.common import DetectMultiBackend
 from utils.dataloaders import LoadImages
@@ -22,8 +22,9 @@ def run_yolov5(source_path,weights_path,conf_thresh,iou_thresh,space_path):
     print("Load time",time.time()-st)
     stride, names, pt = model.stride, model.names, model.pt
     imgsz = check_img_size((608,608), s=stride)
+    # img = cv2.imread(source)
+    # shape = img.shape
     dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=1)
-    vid_path, vid_writer = [None] * bs, [None] * bs
     model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))
     dt = (Profile(), Profile(), Profile())
     for path, im, im0s, vid_cap, s in dataset:
@@ -43,17 +44,21 @@ def run_yolov5(source_path,weights_path,conf_thresh,iou_thresh,space_path):
         with dt[2]:
             pred = non_max_suppression(pred, conf_thres, iou_thres, None, False, max_det=1000)
         
-        for i, det in enumerate(pred):
-            p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]
+        for det in pred:
+            im0 = im0s.copy()
+            # gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]
             if len(det):
                 det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
                 for c in det[:, 5].unique():
                         n = (det[:, 5] == c).sum()  # detections per class
-                        s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "
+                        # s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "
                 for *xyxy, conf, cls in reversed(det):
-                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()
-                    result.append([classes[int(cls)],xywh,float(conf)])
+                    xyxy = list(map(int,xyxy))
+                    # cv2.rectangle(img,(xyxy[0],xyxy[1]),(xyxy[2],xyxy[3]),(0,255,0),2)
+                    result.append([classes[int(cls)],xyxy,float(conf)])
+    # cv2.imshow("frame",img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
     return result
 # res = run_yolov5("testing_images\૦૫૨ 218 ળડશ ળીઅંથ વઅઃપ ૫૨૧ ધઠગ રચસ એભીઅંયં આછૂબ_71.jpg","./YoloV5_model2/yolov5x_latest_8000/weights/best.pt",0.55,0.5,"space.names")
 # print(res)
